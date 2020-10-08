71e100aa5c5f6399bba3d28a0ddb5fcff3b2bdc8
mustafaasadullah <fifafcb00@gmail.com>
2020-10-07 21:11:52 -0700

    homework done

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,23 @@
-public class Problem2 {
+public class Problem2
+{
 
     // Do not change signature (function name, parameters)
-    public static void bubbleSort(int[] data) {
+    public static void bubbleSort(int[] data)
+    {
         // Add your solution
+        //***I WILL BE ASCENDING***
+        int temp = 0;
+        for(int i = 0; i <= data.length; i++)
+        {
+            for (int j = 0; j < data.length - 1 - i; j++)
+            {
+                if(data[j] > data[j + 1])
+                {
+                    temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,26 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int inputs[][] = {
+                {},
+                {3},
+                {3, 1},
+                {8, 5, 12, 2},
+                {21, 12, 1, 16, 20},
+        };
+
+        int expected[][] = {
+                {},
+                {3},
+                {1, 3},
+                {2, 5, 8, 12},
+                {1, 12, 16, 20, 21},
+        };
+
+        assertEquals(inputs.length, expected.length);
+        for (int i = 0; i < inputs.length; i++) {
+            Problem2.bubbleSort(inputs[i]);
+            assertArrayEquals(inputs[i], expected[i]);
+        }
     }
 }
